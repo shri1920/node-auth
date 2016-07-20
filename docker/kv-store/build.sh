57d8f5860029b3125ac4fd41c5ec8ead48508088
Robin <robin.mandangada@mobinius.com>
2016-07-20 19:25:42 +0530

    Dockerizing the Token storage

---
+++ docker/kv-store/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set the Variables
+set -e
+
+WHICH='/usr/bin/which'
+DOCKER=`${WHICH} docker`
+FIGLET=`${WHICH} figlet`
+DATE=`${WHICH} date`
+WHOAMI=`${WHICH} whoami`
+
+# To create the Banner
+if [ -n ${FIGLET} ]; then
+    ${FIGLET} mMolecules Auth-Store  > ./banner.txt
+fi
+
+# To display information about the current build
+echo '\n'
+echo ver mMolecules Auth-Store (Redis) build @ `${DATE}` by `${WHOAMI}`
+echo '\n'
+cat ./banner.txt
+echo '\n'
+
+# To build the Docker image for Key Value store (Redis)
+echo '\n ----- Building the Docker Image : docker.mmolecules/auth-store ----- \n'
+docker build -t docker.mmolecules/auth-store .
+
+echo '\n ----- To start the Docker container ----- \n'
+echo 'docker run --name mmolecules-auth -d docker.mmolecules/auth-store'
+
